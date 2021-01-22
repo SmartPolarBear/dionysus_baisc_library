2e9a2b4ecc597509a5a01b8c9f3b1c3970543271
SmartPolarBear <CleverCoolBear@outlook.com>
2021-01-22 22:59:47 +0800

    ready to implement avl_tree

--- avl_tree.h
+++ avl_tree.h
@@ -3,4 +3,22 @@
 namespace kbl
 {
 
+template<typename T>
+struct avl_tree_head
+{
+
+};
+
+template<typename T, avl_tree_head<T> T::*Link, bool EnableLock = false>
+class avl_tree_iterator
+{
+
+};
+
+template<typename T, avl_tree_head<T> T::*Link, bool EnableLock = false>
+class avl_tree
+{
+public:
+};
+
 }
