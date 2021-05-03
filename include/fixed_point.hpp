705ffb0580d644781fcc0795f055037b746f9618
SmartPolarBear <clevercoolbear@outlook.com>
2021-05-03 22:56:52 +0800

    feat(fixed-point): fixed-point with simple arithmetics

--- include/fixed_point.hpp
+++ include/fixed_point.hpp
@@ -31,70 +31,151 @@ class fixed_point
 	};
 
  public:
+	fixed_point() = default;
+	~fixed_point() = default;
+	fixed_point(const fixed_point&) = default;
+	fixed_point(fixed_point&&) noexcept = default;
+	fixed_point& operator=(const fixed_point&) = default;
 
-	explicit fixed_point(uint32_t n)
+	explicit fixed_point(float n)
+		: data_{ int32_t(n * FACT) }
+	{
+	}
+
+	explicit fixed_point(int32_t n)
 		: data_{ n * FACT }
 	{
-
 	}
 
-	fixed_point add(const fixed_point& another)
+	fixed_point add(const fixed_point& another) const
 	{
-		return fixed_point{ data_ + another.data_ };
+		fixed_point ret{ 0 };
+		ret.data_ = data_ + another.data_;
+		return ret;
 	}
 
-	fixed_point subtract(const fixed_point& another)
+	fixed_point subtract(const fixed_point& another) const
 	{
-		return fixed_point{ data_ - another.data_ };
+		fixed_point ret{ 0 };
+		ret.data_ = data_ - another.data_;
+		return ret;
 	}
 
-	fixed_point add(int32_t n)
+	fixed_point add(int32_t n) const
 	{
-		return fixed_point{ data_ + n * FACT };
+		fixed_point ret{ 0 };
+		ret.data_ = data_ + n * FACT;
+		return ret;
 	}
 
-	fixed_point subtract(int32_t n)
+	fixed_point subtract(int32_t n) const
 	{
-		return fixed_point{ data_ - n * FACT };
+		fixed_point ret{ 0 };
+		ret.data_ = data_ - n * FACT;
+		return ret;
 	}
 
-	fixed_point multiply(const fixed_point& another)
+	fixed_point multiply(const fixed_point& another) const
 	{
-		auto raw = ((int64_t)data_) * another.data_ / FACT;
-		return fixed_point{ raw };
+		fixed_point ret{ 0 };
+		ret.data_ = ((int64_t)data_) * another.data_ / FACT;
+		return ret;
 	}
 
-	fixed_point divide(const fixed_point& another)
+	fixed_point divide(const fixed_point& another) const
 	{
-		auto raw = ((int64_t)data_) * FACT / another.data_;
-		return fixed_point{ raw };
+		fixed_point ret{ 0 };
+		ret.data_ = ((int64_t)data_) * FACT / another.data_;
+		return ret;
 	}
 
-	fixed_point multiply(int32_t n)
+	fixed_point multiply(int32_t n) const
 	{
-		return fixed_point{ data_ * n };
+		fixed_point ret{ 0 };
+		ret.data_ = data_ * n;
+		return ret;
 	}
 
-	fixed_point divide(int32_t n)
+	fixed_point divide(int32_t n) const
 	{
-		return fixed_point{ data_ / n };
+		fixed_point ret{ 0 };
+		ret.data_ = data_ / n;
+		return ret;
 	}
 
-	[[nodiscard]] int32_t to_int(roundings rounding = roundings::ROUND_TO_NEAREST)
+	fixed_point operator+(const fixed_point& another) const
+	{
+		return this->add(another);
+	}
+
+	fixed_point operator+(int32_t another) const
+	{
+		return this->add(another);
+	}
+
+	fixed_point operator-(const fixed_point& another) const
+	{
+		return this->subtract(another);
+
+	}
+
+	fixed_point operator-(int32_t another) const
+	{
+		return this->subtract(another);
+
+	}
+
+	fixed_point operator*(const fixed_point& another) const
+	{
+		return this->multiply(another);
+	}
+
+	fixed_point operator*(int32_t another) const
+	{
+		return this->multiply(another);
+	}
+
+	fixed_point operator/(const fixed_point& another) const
+	{
+		return this->divide(another);
+	}
+
+	fixed_point operator/(int32_t another) const
+	{
+		return this->divide(another);
+	}
+
+	[[nodiscard]] int32_t to_int(roundings rounding = roundings::ROUND_TO_NEAREST) const
 	{
 		if (rounding == roundings::ROUND_TO_NEAREST)
 		{
-
+			return data_ / FACT;
 		}
 		else
 		{
-
+			if (data_ > 0)
+			{
+				return (data_ + FACT / 2) / FACT;
+			}
+			else
+			{
+				return (data_ - FACT / 2) / FACT;
+			}
 		}
 	}
+
+	[[nodiscard]] float to_float() const
+	{
+		return data_ / float(FACT);
+	}
+
+	auto operator<=>(const fixed_point& another) const = default;
  private:
 	static constexpr int32_t FACT = 1 << Q;
 
 	int32_t data_{ 0 };
 };
+
+using fixed_point_17dot14 = fixed_point<17, 14>;
 
 }
--- test/fixed_point_test.cc
+++ test/fixed_point_test.cc
@@ -1,4 +1,43 @@
 #include <cstdint>
 
+#include <gtest/gtest.h>
+
 #include "fixed_point.hpp"
 
+using namespace kbl;
+
+TEST(FixPointTest, Basic)
+{
+	fixed_point_17dot14 f1{ -10 }, f2{ 10 };
+
+	ASSERT_EQ(f1.to_int(), -10);
+	ASSERT_EQ(f2.to_int(), 10);
+
+	auto f3 = f1 + f2;
+
+	ASSERT_EQ(f3.to_int(), 0);
+
+	f1 = f1 / 3;
+	ASSERT_EQ(f1.to_int(), -3);
+	ASSERT_LE(f1.to_float() - (-10.0 / 3), 1e-3);
+
+	f2 = f2 / 3;
+	ASSERT_EQ(f2.to_int(), 3);
+	ASSERT_LE(f2.to_float() - (10.0 / 3), 1e-3);
+
+	f3 = f1 + f2;
+	ASSERT_EQ(f3.to_int(), 0);
+}
+
+TEST(FixPointTest, Floating)
+{
+	fixed_point_17dot14 f1{ 10.25f }, f2{ 0.25f };
+	ASSERT_LE(f1.to_float() - 10.25, 1e-3);
+	ASSERT_LE(f2.to_float() - 0.25, 1e-3);
+
+	auto f3 = f1 - f2;
+	ASSERT_EQ(f3.to_int(), 10);
+
+	auto f4 = f3 * f2;
+	ASSERT_EQ(f4.to_int(), 4);
+}
